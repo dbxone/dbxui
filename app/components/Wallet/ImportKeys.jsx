d69ec858588d4d655bfd5984a469f0b9e139ea97
Li RuiGang <yueyemingming@163.com>
2018-06-22 15:21:43 +0800

    update app/components/Wallet/ImportKeys.jsx

--- app/components/Wallet/ImportKeys.jsx
+++ app/components/Wallet/ImportKeys.jsx
@@ -329,7 +329,7 @@ class ImportKeys extends Component {
                 }
             }
             if (!encrypted_brainkey)
-                throw "Please use a BTS 1.0 wallet_export_keys file instead";
+                throw "Please use a DBX 1.0 wallet_export_keys file instead";
 
             if (!password_checksum)
                 throw file.name + " is missing password_checksum";
